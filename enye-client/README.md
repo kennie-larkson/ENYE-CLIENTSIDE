0dc36961f4ee355edd0b461bf7aa59c18a190439
LAWAL ABD'KEHINDE <32939546+kennie-larkson@users.noreply.github.com>
2021-01-12 13:57:14 +0100

    updated README

--- enye-client/README.md
+++ enye-client/README.md
@@ -6,6 +6,10 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 In the project directory, you can run:
 
+### `cd enye-client`
+
+Then run the following command to start the application:
+
 ### `yarn start`
 
 Runs the app in the development mode.\
--- enye-client/src/components/searchbar.js
+++ enye-client/src/components/searchbar.js
@@ -9,25 +9,12 @@ const SearchBar = ({ profiles }) => {
     setInputValue(value);
   };
 
-  // console.log(inputValue.toLocaleLowerCase());
-  // console.log(profiles.map(profile => Object.keys(profile)));
+  
   const profile_keys = profiles.map((profile) => Object.values(profile));
-  // const filteredData = profile_keys.filter(
-  //   (profile_key) => profile_key.value === inputValue
-  // );
+  
   console.log(profile_keys);
 
-  //button click event handler
-  // let filteredData = [];
-  // const handleClick = (event) => {
-  //   event.preventDefault();
-  //   const lowerCasedFilter = inputValue.toLowerCase();
-  //   filteredData = profiles.filter((profile) => {
-  //     return Object.keys(profile).some((key) => {
-  //       return profile[key].toLowerCase().includes(lowerCasedFilter);
-  //     });
-  //   });
-  // };
+
 
   return (
     <main>
@@ -48,15 +35,7 @@ const SearchBar = ({ profiles }) => {
         >
           Search
         </button>
-        {/* </form> */}
-        {/* {filteredData.map((profile) => (
-          <div key={profile.CreditCardNumber}>
-            <div>
-              {profile.FirstName} {profile.LastName} - {profile.gender} -{" "}
-              {profile.CreditCardType}
-            </div>
-          </div>
-        ))} */}
+        
       </nav>
     </main>
   );
